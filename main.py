28bac746e4c2c3ba6a4fbc544509740b8a28242f
Panuthep <falcon_270394@hotmail.co.th>
2019-07-27 11:18:30 +0700

    Update to support input by hand

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from subprocess import PIPE
 from os.path import join
 
 
-def run(exe_dir, input):
+def run(exe_dir, input=None):
+	"""
+	Leave input to None if you want to input by hand.
+	"""
 	if os.path.exists(exe_dir):
 		process = subprocess.run(join("./", exe_dir), stdout=PIPE, input=input, encoding="utf-8")
 		
@@ -13,6 +16,7 @@ def run(exe_dir, input):
 
 if __name__ == "__main__":
 	# Testing
+	print(run("Executables/getCharecter"))
 	print(run("Executables/getCharecter", "A"))
 	print(run("Executables/getNumber", "15"))
 	print(run("Executables/getString", "University"))
